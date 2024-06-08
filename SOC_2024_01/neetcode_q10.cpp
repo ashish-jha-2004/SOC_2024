881626a992648c8fed6d63b412d444c16cf8d6dc
Ashish Kumar <152195795+ashish-jha-2004@users.noreply.github.com>
2024-06-08 10:48:13 +0530

    Delete SOC_2024_01 directory

--- SOC_2024_01/neetcode_q10.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        if (*lower_bound(nums.begin(), nums.end(), target) != target) return -1;
-        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-    }
-};
-
